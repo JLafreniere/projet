5455619f2e4237c8bcfa47740c8671021ee45587
Jonathan Lafrenière <jonathan.lafreniere.02@edu.cegeptr.qc.ca>
2016-11-01 14:27:55 -0400

    Intégration

--- Projet_Dietetique/frmVoirRecettes.Designer.vb
+++ Projet_Dietetique/frmVoirRecettes.Designer.vb
@@ -23,17 +23,148 @@ Partial Class frmVoirRecettes
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmVoirRecettes))
+        Me.mainpanel = New System.Windows.Forms.Panel()
+        Me.btnAjouter = New System.Windows.Forms.Button()
+        Me.btnModifier = New System.Windows.Forms.Button()
+        Me.btnSupprimer = New System.Windows.Forms.Button()
+        Me.lsvRecette = New System.Windows.Forms.ListView()
+        Me.ColumnHeader1 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader2 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.ColumnHeader3 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.BtnRechercher = New System.Windows.Forms.Button()
+        Me.txtRecherche = New System.Windows.Forms.TextBox()
+        Me.mainpanel.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'mainpanel
+        '
+        Me.mainpanel.Controls.Add(Me.btnAjouter)
+        Me.mainpanel.Controls.Add(Me.btnModifier)
+        Me.mainpanel.Controls.Add(Me.btnSupprimer)
+        Me.mainpanel.Controls.Add(Me.lsvRecette)
+        Me.mainpanel.Controls.Add(Me.BtnRechercher)
+        Me.mainpanel.Controls.Add(Me.txtRecherche)
+        Me.mainpanel.Location = New System.Drawing.Point(491, 252)
+        Me.mainpanel.Name = "mainpanel"
+        Me.mainpanel.Size = New System.Drawing.Size(702, 483)
+        Me.mainpanel.TabIndex = 1
+        '
+        'btnAjouter
+        '
+        Me.btnAjouter.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnAjouter.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(176, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.btnAjouter.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnAjouter.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAjouter.ForeColor = System.Drawing.Color.White
+        Me.btnAjouter.Location = New System.Drawing.Point(398, 430)
+        Me.btnAjouter.Name = "btnAjouter"
+        Me.btnAjouter.Size = New System.Drawing.Size(91, 35)
+        Me.btnAjouter.TabIndex = 58
+        Me.btnAjouter.Text = "Ajouter"
+        Me.btnAjouter.UseVisualStyleBackColor = False
+        '
+        'btnModifier
+        '
+        Me.btnModifier.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnModifier.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(176, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.btnModifier.Enabled = False
+        Me.btnModifier.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnModifier.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnModifier.ForeColor = System.Drawing.Color.White
+        Me.btnModifier.Location = New System.Drawing.Point(495, 430)
+        Me.btnModifier.Name = "btnModifier"
+        Me.btnModifier.Size = New System.Drawing.Size(91, 35)
+        Me.btnModifier.TabIndex = 59
+        Me.btnModifier.Text = "Modifier"
+        Me.btnModifier.UseVisualStyleBackColor = False
+        '
+        'btnSupprimer
+        '
+        Me.btnSupprimer.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSupprimer.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(176, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.btnSupprimer.Enabled = False
+        Me.btnSupprimer.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSupprimer.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSupprimer.ForeColor = System.Drawing.Color.White
+        Me.btnSupprimer.Location = New System.Drawing.Point(592, 430)
+        Me.btnSupprimer.Name = "btnSupprimer"
+        Me.btnSupprimer.Size = New System.Drawing.Size(91, 35)
+        Me.btnSupprimer.TabIndex = 60
+        Me.btnSupprimer.Text = "Supprimer"
+        Me.btnSupprimer.UseVisualStyleBackColor = False
+        '
+        'lsvRecette
+        '
+        Me.lsvRecette.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader1, Me.ColumnHeader2, Me.ColumnHeader3})
+        Me.lsvRecette.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lsvRecette.HideSelection = False
+        Me.lsvRecette.Location = New System.Drawing.Point(5, 52)
+        Me.lsvRecette.MultiSelect = False
+        Me.lsvRecette.Name = "lsvRecette"
+        Me.lsvRecette.Size = New System.Drawing.Size(697, 372)
+        Me.lsvRecette.TabIndex = 57
+        Me.lsvRecette.UseCompatibleStateImageBehavior = False
+        Me.lsvRecette.View = System.Windows.Forms.View.Details
+        '
+        'ColumnHeader1
+        '
+        Me.ColumnHeader1.Text = "Nom Recette"
+        Me.ColumnHeader1.Width = 305
+        '
+        'ColumnHeader2
+        '
+        Me.ColumnHeader2.Text = "Temps de préparation"
+        Me.ColumnHeader2.Width = 178
+        '
+        'ColumnHeader3
+        '
+        Me.ColumnHeader3.Text = "Temps de refroidissement"
+        Me.ColumnHeader3.Width = 197
+        '
+        'BtnRechercher
+        '
+        Me.BtnRechercher.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnRechercher.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(176, Byte), Integer), CType(CType(240, Byte), Integer))
+        Me.BtnRechercher.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnRechercher.Font = New System.Drawing.Font("Segoe UI", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnRechercher.ForeColor = System.Drawing.Color.White
+        Me.BtnRechercher.Location = New System.Drawing.Point(510, 11)
+        Me.BtnRechercher.Name = "BtnRechercher"
+        Me.BtnRechercher.Size = New System.Drawing.Size(91, 35)
+        Me.BtnRechercher.TabIndex = 56
+        Me.BtnRechercher.Text = "Rechercher"
+        Me.BtnRechercher.UseVisualStyleBackColor = False
+        '
+        'txtRecherche
+        '
+        Me.txtRecherche.Location = New System.Drawing.Point(320, 19)
+        Me.txtRecherche.Name = "txtRecherche"
+        Me.txtRecherche.Size = New System.Drawing.Size(184, 20)
+        Me.txtRecherche.TabIndex = 0
         '
         'frmVoirRecettes
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1684, 987)
+        Me.Controls.Add(Me.mainpanel)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frmVoirRecettes"
         Me.Text = "Recettes"
+        Me.mainpanel.ResumeLayout(False)
+        Me.mainpanel.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents mainpanel As Panel
+    Friend WithEvents btnAjouter As Button
+    Friend WithEvents btnModifier As Button
+    Friend WithEvents btnSupprimer As Button
+    Friend WithEvents lsvRecette As ListView
+    Friend WithEvents ColumnHeader1 As ColumnHeader
+    Friend WithEvents ColumnHeader2 As ColumnHeader
+    Friend WithEvents ColumnHeader3 As ColumnHeader
+    Friend WithEvents BtnRechercher As Button
+    Friend WithEvents txtRecherche As TextBox
 End Class
--- Projet_Dietetique/frmVoirRecettes.vb
+++ Projet_Dietetique/frmVoirRecettes.vb
@@ -1,10 +1,184 @@
-﻿Public Class frmVoirRecettes
-    Private Sub frmVoirRecettes_Load(sender As Object, e As EventArgs) Handles MyBase.Shown
+﻿Imports System.Data
+Imports MySql.Data.MySqlClient
+Public Class frmVoirRecettes
+    'Jonathan Villeneuve
+    Dim bd As New GestionBD("Server=localhost;Database=bd_application;Uid=root;Pwd=;")
+    Dim dsRecettes As DataSet
+    Dim daRecettes As New MySqlDataAdapter
+    Dim dsProduits As DataSet
+    Dim daProduits As New MySqlDataAdapter
+    Dim itemcoll(3) As String
+    Dim position As Integer
+    Dim allergies() As String
+    Dim portions(2) As String
+    Dim conservation() As String
+    Dim refroidissement() As String
+
+    Private Sub frmVoirRecettes_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Ajoute l'entête avec les onglets
         Controls.Add(New Header(Me, True))
-        Me.BackColor = Color.White
+        bd.ConnectionString = "Server=localhost; DataBase=bd_application;UId=root;Pwd=; Convert Zero Datetime=true; Allow Zero DateTime=true;"
+
+        centerControl(mainpanel)
+        chargerdataset()
+        remplirListView()
     End Sub
 
-    Private Sub frmVoirRecettes_Load_1(sender As Object, e As EventArgs) Handles MyBase.Load
+    'Centre le panel au centre de la page
+    Private Sub centerControl(c As Control)
+        c.Left = (Me.ClientSize.Width / 2) - (c.Width / 2)
+    End Sub
 
+    Private Sub btnAjouter_Click(sender As Object, e As EventArgs) Handles btnAjouter.Click
+        Hide()
+        frmAjoutRecettes.Show()
+
+
+    End Sub
+    Sub chargerdataset() 'Rempli les dataset produits et recettes
+        bd.dsRecettes.Clear()
+        bd.dsProduits.Clear()
+        bd.dsDetails.Clear()
+        bd.Requete("select * from recettes order by nom", bd.dsRecettes, bd.daRecettes, "recettes")
+        bd.Requete("select * from produits ", bd.dsProduits, bd.daProduits, "produits")
+        bd.Requete("select * from details_recette", bd.dsDetails, bd.daDetails, "details_recette")
+    End Sub
+    'Rempli le listview avec les données de la table recettes
+    Sub remplirListView()
+        lsvRecette.Items.Clear()
+        For i = 0 To bd.dsRecettes.Tables(0).Rows.Count - 1
+            itemcoll(0) = bd.dsRecettes.Tables(0).Rows(i)(1).ToString
+            itemcoll(1) = bd.dsRecettes.Tables(0).Rows(i)(2).ToString
+            itemcoll(2) = bd.dsRecettes.Tables(0).Rows(i)(8).ToString
+            itemcoll(3) = bd.dsRecettes.Tables(0).Rows(i)(12).ToString
+
+            Dim lvi As New ListViewItem(itemcoll)
+            lsvRecette.Items.Add(lvi)
+        Next
+    End Sub
+
+    Private Sub btnModifier_Click(sender As Object, e As EventArgs) Handles btnModifier.Click
+        remplirFormulaire()
+
+    End Sub
+
+    Private Sub lsvRecette_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lsvRecette.SelectedIndexChanged
+        'Renvoie l'Indice de l'élément sélectionné dans le ListView
+        If lsvRecette.SelectedItems.Count > 0 Then
+            btnModifier.Enabled = True
+            btnSupprimer.Enabled = True
+            position = lsvRecette.SelectedIndices(0)
+        Else
+            btnSupprimer.Enabled = False
+            btnModifier.Enabled = False
+        End If
+    End Sub
+
+    'Affecte aux controles de la form frmRecettes les données de l'élément du ListView Sélectionné
+    Sub remplirFormulaire()
+        frmAjoutRecettes.Show()
+        frmAjoutRecettes.txtId.Text = bd.dsRecettes.Tables(0).Rows(position).Item(0).ToString
+        frmAjoutRecettes.txtNom.Text = bd.dsRecettes.Tables(0).Rows(position).Item(1).ToString
+        frmAjoutRecettes.txtPreparation.Text = bd.dsRecettes.Tables(0).Rows(position).Item(2).ToString
+        frmAjoutRecettes.txtCuisson.Text = bd.dsRecettes.Tables(0).Rows(position).Item(3).ToString
+        frmAjoutRecettes.txtFaraneith.Text = bd.dsRecettes.Tables(0).Rows(position).Item(7).ToString
+        frmAjoutRecettes.nudPortions.Value = bd.dsRecettes.Tables(0).Rows(position).Item(4).ToString
+        frmAjoutRecettes.txtCategorie.Text = bd.dsRecettes.Tables(0).Rows(position).Item(14).ToString
+        ' On coche ou  le checkBox si la recette est congelable
+        If bd.dsRecettes.Tables(0).Rows(position).Item(15).ToString = True Then
+            frmAjoutRecettes.chkCongelable.Checked = True
+        Else
+            frmAjoutRecettes.chkCongelable.Checked = False
+        End If
+        frmAjoutRecettes.txtEtapes.Text = bd.dsRecettes.Tables(0).Rows(position).Item(9).ToString
+        frmAjoutRecettes.txtRemarques.Text = bd.dsRecettes.Tables(0).Rows(position).Item(11).ToString
+        'On ajoute les allergies dans le listbox
+        allergies = bd.dsRecettes.Tables(0).Rows(position).Item(12).ToString.Split(vbCrLf)
+        For i As Integer = 0 To allergies.Length - 1
+            frmAjoutRecettes.lstAllergies.Items.Add(allergies(i))
+        Next
+
+        portions = bd.dsRecettes.Tables(0).Rows(position).Item(5).ToString.Split(" ")
+
+        frmAjoutRecettes.txtPortions.Text = portions(0)
+        frmAjoutRecettes.cbPortions.Text = portions(1)
+        conservation = bd.dsRecettes.Tables(0).Rows(position).Item(13).ToString.Split(" ")
+        frmAjoutRecettes.txtConservation.Text = conservation(0)
+        frmAjoutRecettes.cbConservation.Text = conservation(1)
+        refroidissement = bd.dsRecettes.Tables(0).Rows(position).Item(8).ToString.Split(" ")
+        frmAjoutRecettes.txtRefroid.Text = refroidissement(0)
+        frmAjoutRecettes.cbRefroid.Text = refroidissement(1)
+        frmAjoutRecettes.btnEnregistrer.Text = "Modifier"
+
+        frmAjoutRecettes.remplirListView()
+
+    End Sub
+
+    Private Sub btnSupprimer_Click(sender As Object, e As EventArgs) Handles btnSupprimer.Click 'Supprime la recette sélectionée et les données de la table détails_recettes liées à cette recette
+        Dim ds As New DataSet
+        Dim ds2 As New DataSet
+        If MsgBox("Voulez-vous supprimer cette recette?", MsgBoxStyle.YesNo, "Confirmation") = MsgBoxResult.Yes Then
+            For Each element As ListViewItem In lsvRecette.SelectedItems
+
+                bd.Requete("Select * from recettes where nom = '" & element.SubItems(0).Text & "'", ds, bd.daRecettes, "recettes")
+
+                bd.Requete("Select * from details_recette where id_recette =  " & ds.Tables(0).Rows(0).Item(0).ToString & " ", ds2, bd.daDetails, "details_recette")
+
+                For i As Integer = 0 To ds2.Tables(0).Rows.Count - 1
+                    ds2.Tables(0).Rows(i).Delete()
+
+                Next
+                bd.miseAjourBD(bd.dsDetails, bd.daDetails, "details_recette")
+            Next
+
+            ds.Tables(0).Rows(0).Delete()
+
+
+
+        End If
+        btnSupprimer.Enabled = False
+        btnModifier.Enabled = False
+
+
+        bd.miseAjourBD(bd.dsRecettes, bd.daRecettes, "recettes")
+
+        remplirListView()
+
+
+
+    End Sub
+    'Recherche dans la BD les recettes selon le contenu du Textbox
+    Sub recherche()
+        lsvRecette.Enabled = True
+        bd.dsRecettes.Clear()
+        If (txtRecherche.Text = "") Then
+            bd.Requete("select * from recettes order by nom", bd.dsRecettes, bd.daRecettes, "recettes")
+        Else
+            bd.Requete("Select * from recettes where lower(nom) like lower('" & Replace(txtRecherche.Text, "'", "''") & "%')  order by nom", bd.dsRecettes, bd.daRecettes, "recettes")
+        End If
+
+        remplirListView()
+
+        If lsvRecette.Items.Count = 0 Then
+            lsvRecette.Items.Add("Aucune recette trouvée")
+            lsvRecette.Enabled = False
+
+            bd.Requete("select * from recettes order by nom", bd.dsRecettes, bd.daRecettes, "recettes")
+        End If
+
+        btnSupprimer.Enabled = False
+        btnModifier.Enabled = False
+        txtRecherche.Text = ""
+    End Sub
+
+    Private Sub BtnRechercher_Click(sender As Object, e As EventArgs) Handles BtnRechercher.Click
+        recherche()
+    End Sub
+    'Fait la recherche si l'utilisateur appuie sur enter
+    Private Sub txtRecherche_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtRecherche.KeyPress
+        If e.KeyChar = Chr(13) Then
+            recherche()
+
+        End If
     End Sub
 End Class

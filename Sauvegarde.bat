12f3d8c2ea1a2ed0315904251e164c12da61ff9c
Jonathan Lafreniere <Jonathan.lafreniere@hotmail.com>
2016-11-01 15:39:26 -0400

    Mod

--- Sauvegarde.bat
+++ Sauvegarde.bat
@@ -7,9 +7,33 @@ git pull
 git push
 echo.
 echo.
-echo.
+echo --------------------
 echo FIN DE LA SAUVEGARDE
+echo --------------------
 echo.
 echo.
-echo.
+echo              _ _,---._ 
+echo           ,-','       `-.___ 
+echo          /-;'               `._ 
+echo         /\/          ._   _,'o \ 
+echo        ( /\       _,--'\,','"`. ) 
+echo         ^|\      ,'o     \'    //\ 
+echo         ^|      \        /   ,--'""`-. 
+echo         :       \_    _/ ,-'         `-._ 
+echo          \        `--'  /                ) 
+echo           `.  \`._    ,'     ________,',' 
+echo             .--`     ,'  ,--` __\___,;' 
+echo              \`.,-- ,' ,`_)--'  /`.,' 
+echo               \^( ;  ^| ^| )      ^(`-/ 
+echo                 `--'^| ^|^)       ^|-/ 
+echo                   ^| ^| ^|        ^| ^| 
+echo                   ^| ^| ^|,.,-.   ^| ^|_ 
+echo                   ^| `./ /   )---`  ) 
+echo                  _^|  /    ,',   ,-' 
+echo                 ,'^|_(    /-^<._,' ^|--, 
+echo                 ^|    `--'---.     \/ \ 
+echo                 ^|          / \    /\  \ 
+echo               ,-^---._     ^|  \  /  \  \ 
+echo            ,-'        \----'   \/    \--`. 
+echo           /            \              \   \ 
 pause
